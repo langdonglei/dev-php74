3069f6beceecd3b450b911b23cbdbc428379dea9
langdonglei <langdonglei@icloud.com>
2025-06-17 22:08:04 +0800

    build(install): 修改 pdo_sqlsrv 扩展版本
    - 将 pdo_sqlsrv 扩展版本从 5.11.1降级到 5.10.1
    - 此修改可能与特定 PHP 版本或其他依赖项兼容性有关

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ printf '\n' | pecl install redis-6.2.0 && docker-php-ext-enable redis
 curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.10.6.1-1_amd64.apk
 printf '\n' | apk add --allow-untrusted msodbcsql17_17.10.6.1-1_amd64.apk
 apk add unixodbc-dev
-pecl install pdo_sqlsrv-5.11.1
+pecl install pdo_sqlsrv-5.10.1
 docker-php-ext-enable pdo_sqlsrv
 docker-php-ext-install pdo_mysql
